440bf72847ecf0aa972c23528aa4c7fdcaee6b58
ricardosantanadev <ricardosantanadev@gmail.com>
2023-04-25 12:47:29 -0300

    update

--- src/app/courses/containers/courses-form/courses-form.component.ts
+++ src/app/courses/containers/courses-form/courses-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Courses } from '../../model/courses';
@@ -13,12 +13,11 @@ import { CourseServiceService } from '../../service/course.service';
 })
 export class CoursesFormComponent {
   // um grupo de campos de um formulario e chamado de FormGroup
-  // form: UntypedFormGroup;
-  form = this.formBuilder.group({
+  coursesForm = this.formBuilder.group({
     // campos do formulario
     id: [''],
-    name: [''],
-    category: [''],
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+    category: ['', [Validators.required]],
   });
 
   // para criar o formulario de cadastro e necessario ter as classes FormBuilder e FormGroup. com isso e necessario importar o modulo ReactiveFormsModule
@@ -26,7 +25,7 @@ export class CoursesFormComponent {
   constructor(private formBuilder: NonNullableFormBuilder, private courseService: CourseServiceService
     , private _snackBar: MatSnackBar, private location: Location, private route: ActivatedRoute) {
 
-    // this.form = this.formBuilder.group({
+    // this.coursesForm = this.formBuilder.group({
     //   // campos do formulario
     //   name: [''],
     //   category: [''l]
@@ -34,19 +33,22 @@ export class CoursesFormComponent {
 
     const course: Courses = this.route.snapshot.data['course'];
     console.log(course);
-    this.form.setValue({
+    // seta o que foi retornado no resolver
+    this.coursesForm.setValue({
       id: course.id,
       name: course.name,
       category: course.category,
     })
-    // console.log(this.form.value)
-
+    // console.coursesForm(this.form.value)
   }
 
   onSubmit() {
-    // console.log(this.form.value);
+    if(this.coursesForm.invalid){
+      return ;
+    }
+    // console.log(this.coursesForm.value);
     // necessario se increver no observable para funcionar usando o subscribe()
-    this.courseService.save(this.form.value).subscribe({ next: sucess => this.onSucess('Curso salvo com sucesso!'), error: error => this.onError('Erro ao salvar curso') });
+    this.courseService.save(this.coursesForm.value).subscribe({ next: sucess => this.onSucess('Curso salvo com sucesso!'), error: error => this.onError('Erro ao salvar curso') });
   }
 
   onCancel() {
@@ -69,5 +71,33 @@ export class CoursesFormComponent {
       horizontalPosition: 'right',
       verticalPosition: 'top',
     });
+  }
+
+  getErrorMessage(fieldName: string) {
+    // .get('') metodo usado para referenciar o campo do formulario quando o formulario e do tipo .group
+    const field = this.coursesForm.get(fieldName);
+    // operador elvis ? se o campo tiver nullo ou  nao foi preenchido  ele nao vai apresentar erro
+    // required valida se o campo estiver preechido;
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+    // o nome da validacao tem que ser tudo menusculo para poder funcionar
+    if (field?.hasError('minlength')) {
+      // pega o tamanho necessario de caracters para poder informar qual e a quantidade de caracters
+      //  field.errors se o erro existir ? field.errors['minlength'] ['requiredLength']obtem qual e o erro nesse caso acessou as propriedades  ['minlength'] e a outra propriedade ['requiredLength'] que e o tamanho requerido
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `O tamanho mínimo precisa ser de ${requiredLength} caracteres`;
+    }
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
+      return `O tamanho máximo precisa ser de ${requiredLength} caracteres`;
+    }
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    return 'erro';
+
   }
 }
