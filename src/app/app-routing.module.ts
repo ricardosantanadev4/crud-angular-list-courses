57e864aa2d0981f303522401f223ca4a9a83d5b4
ricardosantanadev <ricardosantanadev@gmail.com>
2023-08-17 16:10:55 -0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // path: '' significa que o caminha esta vazio no caso so seria http://localhost:4200/
+  // pathMatch: 'full' verifica todo o roteamento http://localhost:4200 ou http://localhost:4200 para poder fazer o direcionamento para courses
+  //  redirectTo: 'courses' faz o redirecionamento para a rota courses
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
   {
     path: 'courses',
