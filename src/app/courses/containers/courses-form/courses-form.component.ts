28149a5a2465ca463247882ff9d18d8065629280
ricardosantanadev <ricardosantanadev@gmail.com>
2023-09-15 10:48:14 -0300

    atualizacao dos comentarios e organizacao em courses-form.component

--- src/app/courses/containers/courses-form/courses-form.component.ts
+++ src/app/courses/containers/courses-form/courses-form.component.ts
@@ -15,48 +15,48 @@ import { CourseServiceService } from '../../service/course.service';
 export class CoursesFormComponent {
 
   // um grupo de campos de um formulario e chamado de FormGroup
-  // esse procedimento foi feito em aulas anteriores para tipar os campos do formulario como o 
-  // FormArray no angular poder ser um grupo de FormControls o angular nao trabalha com formularios 
-  // tipados em FormArrays
-  // coursesForm = this.formBuilder.group({
-  //   // inicializando os campos do formulario com vasio e tipando os campos
-  //   id: [''],
-  //   name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-  //   category: ['', [Validators.required]],
-  // });
+  /* esse procedimento foi feito em aulas anteriores para tipar os campos do formulario como o 
+     FormArray no angular poder ser um grupo de FormControls o angular nao trabalha com formularios 
+     tipados em FormArrays */
+  /* coursesForm = this.formBuilder.group({
+     // inicializando os campos do formulario com vasio e tipando os campos
+     id: [''],
+     name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+     category: ['', [Validators.required]],
+   }); */
 
   // ! permite declarar a variavel sem precisar inicializar
   coursesForm!: FormGroup;
 
-  // para criar o formulario de cadastro e necessario ter as classes FormBuilder e FormGroup. 
-  // com isso e necessario importar o modulo ReactiveFormsModule
-  // a classe FormBuilder vai ser usada para auxiliar na criacao de um FormGroup
-  // NonNullableFormBuilder siguinifica que os campos dormulario nao podem ser nulos
+  /* para criar o formulario de cadastro e necessario ter as classes FormBuilder e FormGroup. 
+     com isso e necessario importar o modulo ReactiveFormsModule
+     a classe FormBuilder vai ser usada para auxiliar na criacao de um FormGroup
+     NonNullableFormBuilder siguinifica que os campos dormulario nao podem ser nulos */
   constructor(private formBuilder: NonNullableFormBuilder, private courseService: CourseServiceService
     , private _snackBar: MatSnackBar, private location: Location, private route: ActivatedRoute) {
 
-    // this.coursesForm = this.formBuilder.group({
-    //   campos do formulario
-    //   name: [''],
-    //   category: [''l]
-    // });
+    /* this.coursesForm = this.formBuilder.group({
+       campos do formulario
+       name: [''],
+       category: [''l]
+     }); */
 
     const course: Course = this.route.snapshot.data['course'];
     // console.log(course.lessons);
 
     // seta o que foi retornado no resolver
-    // this.coursesForm.setValue({
-    //   id: course.id,
-    //   name: course.name,
-    //   category: course.category,
-    // })
+    /* this.coursesForm.setValue({
+       id: course.id,
+       name: course.name,
+       category: course.category,
+     }) */
     // console.log(this.coursesForm.value);
 
-    //  aqui e feito a declaracao e inializacao e tipagem do formulario ao mesmo tempo 
-    //  para poder utilizar o FormArray
+    /*  aqui e feito a declaracao e inializacao e tipagem do formulario ao mesmo tempo 
+        para poder utilizar o FormArray */
     this.coursesForm = this.formBuilder.group({
-      // id: new FormControl([course.id]) e a mesma coisa que id: [course.id] 
-      // por isso os campos sao declarados da forma mais simples id: [course.id]
+      /* id: new FormControl([course.id]) e a mesma coisa que id: [course.id] 
+         por isso os campos sao declarados da forma mais simples id: [course.id] */
       id: [course.id],
       name: [course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
       category: [course.category, [Validators.required]],
@@ -71,13 +71,13 @@ export class CoursesFormComponent {
     // console.log(this.coursesForm.value);
   }
 
-  // esse metodo retorna um array de FormGroup, porem o Array de FormGroup e apenas uma lista nao e um
-  // controle do Angular para tornalo um controle vai ser necessario usar um FormArray
+  /* esse metodo retorna um array de FormGroup, porem o Array de FormGroup e apenas uma lista nao e um
+     controle do Angular para tornalo um controle vai ser necessario usar um FormArray */
   private retrieveLesson(course: Course) {
     const lessons = [];
 
-    // a variavel course pode ser nula e  quando tentar acessar lessons vai dar erro ex: null.lessons 
-    // por isso ? o uso do operador Elves
+    /* a variavel course pode ser nula e  quando tentar acessar lessons vai dar erro ex: null.lessons 
+       por isso o uso do operador Elves ?. */
     // se o array tiver aulas executa o if, e se estiver vasio executa o else
     if (course?.lessons) {
       course.lessons.forEach(l => lessons.push(this.createLesson(l)));
@@ -102,9 +102,14 @@ export class CoursesFormComponent {
     }
     // console.log(this.coursesForm.value);
     // necessario se increver no observable para funcionar usando o subscribe()
-    this.courseService.save(this.coursesForm.value).subscribe({ next: sucess => this.onSucess('Curso salvo com sucesso!'), error: error => this.onError('Erro ao salvar curso') });
+    this.courseService.save(this.coursesForm.value).subscribe({
+      next: () =>
+        this.onSucess('Curso salvo com sucesso!'), error: () => this.onError('Erro ao salvar curso')
+    });
   }
 
+  /* no lugar de usar router.navigate para voltar a tela inicial poder ser usado o location: Location
+     faz o mesmo efeito porem com o location utliza menos codigo */
   onCancel() {
     // console.log('Cancel');
     this.location.back();
@@ -128,20 +133,26 @@ export class CoursesFormComponent {
   }
 
   getErrorMessage(fieldName: string) {
-    // .get('') metodo usado para referenciar o campo do formulario quando o formulario e do tipo .group
+    // .get() metodo usado para referenciar o campo do formulario quando o formulario e do tipo .group
     const field = this.coursesForm.get(fieldName);
-    // operador elvis ? se o campo tiver nullo ou  nao foi preenchido  ele nao vai apresentar erro
+
+    // operador elvis ?. se o campo tiver nullo ou  nao foi preenchido  ele nao vai apresentar erro
     // required valida se o campo estiver preechido;
     if (field?.hasError('required')) {
       return 'Campo obrigatório';
     }
+
     // o nome da validacao tem que ser tudo menusculo para poder funcionar
     if (field?.hasError('minlength')) {
-      // pega o tamanho necessario de caracters para poder informar qual e a quantidade de caracters
-      //  field.errors se o erro existir ? field.errors['minlength'] ['requiredLength']obtem qual e o erro nesse caso acessou as propriedades  ['minlength'] e a outra propriedade ['requiredLength'] que e o tamanho requerido
+      /* o doido a baixo pega o tamanho necessario de caracters para poder informar qual e a quantidade 
+      de caracters */
+      /*  field.errors se o erro existir ? field.errors['minlength'] ['requiredLength']obtem qual e o 
+      erro nesse caso acessou as propriedades  ['minlength'] e a outra propriedade ['requiredLength'] 
+      que e o tamanho requerido */
       const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
       return `O tamanho mínimo precisa ser de ${requiredLength} caracteres`;
     }
+
     if (field?.hasError('maxlength')) {
       const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
       return `O tamanho máximo precisa ser de ${requiredLength} caracteres`;
